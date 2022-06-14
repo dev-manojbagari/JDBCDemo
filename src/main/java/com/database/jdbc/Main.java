8db3d300a36b3b7f462b56c497a59f61888c64d5
manojbagari <targetforias@gmail.com>
2022-06-14 06:03:12 +0530

    Sample code to run static sql query with jdbc

--- src/main/java/com/database/jdbc/Main.java
+++ src/main/java/com/database/jdbc/Main.java
@@ -4,25 +4,39 @@ package com.database.jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Main {
 
 	private static final String USERNAME = "dbuser";
 	private static final String PASSWORD = "dbpassword";
 	private static final String CONN_STRING =
-			"jdbc:hsqldb:data/explorecalifornia"; // for in memory db no // required
+			"jdbc:hsqldb:data/explorecalifornia";
 	
 	public static void main(String[] args) throws SQLException {
 		
-		
 		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
 		
 		try {
 			conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
-			System.out.println("Connected!");
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("SELECT coutn(*) FROM states");
+			
+			rs.next();
+			System.out.println("Number of rows: " + rs.getRow());
+			
 		} catch (SQLException e) {
 			System.err.println(e);
 		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			if (stmt != null) {
+				stmt.close();
+			}
 			if (conn != null) {
 				conn.close();
 			}
@@ -31,4 +45,5 @@ public class Main {
 	}
 
 }
+
 
