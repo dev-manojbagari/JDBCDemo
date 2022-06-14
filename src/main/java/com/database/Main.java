d4fecad307cd13071bfb525b57806cb2ffd70a29
manojbagari <targetforias@gmail.com>
2022-06-14 11:51:23 +0530

    Tabel data to Java beans example part 2 (fetching data into beans + inserting data from beans to tables )

--- src/main/java/com/database/Main.java
+++ src/main/java/com/database/Main.java
@@ -2,13 +2,25 @@ package com.database;
 
 import java.sql.SQLException;
 
+import com.database.beans.Admin;
 import com.database.tables.AdminManager;
+import com.database.util.InputHelper;
 
 public class Main {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws Exception {
 
 		AdminManager.displayAllRows();
-
+		
+		Admin bean = new Admin();
+		bean.setUserName(InputHelper.getInput("User name: "));
+		bean.setPassword(InputHelper.getInput("Password: "));
+		
+		boolean result = AdminManager.insert(bean);
+		
+		if (result) {
+			System.out.println("New row with primary key " + bean.getAdminId() + " was inserted!");
+		}
 	}
+
 }
--- src/main/java/com/database/tables/AdminManager.java
+++ src/main/java/com/database/tables/AdminManager.java
@@ -1,18 +1,20 @@
 package com.database.tables;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 import com.database.DBType;
 import com.database.DBUtil;
+import com.database.beans.Admin;
 
 
 public class AdminManager {
 
 	public static void displayAllRows() throws SQLException {
-		
+
 		String sql = "SELECT adminId, userName, password FROM admin";
 		try (
 				Connection conn = DBUtil.getConnection(DBType.MYSQL);
@@ -30,4 +32,71 @@ public class AdminManager {
 			}
 		}
 	}
+
+	public static Admin getRow(int adminId) throws SQLException {
+
+		String sql = "SELECT * FROM admin WHERE adminId = ?";
+		ResultSet rs = null;
+
+		try (
+				Connection conn = DBUtil.getConnection(DBType.MYSQL);
+				PreparedStatement stmt = conn.prepareStatement(sql);
+				){
+			stmt.setInt(1, adminId);
+			rs = stmt.executeQuery();
+
+			if (rs.next()) {
+				Admin bean = new Admin();
+				bean.setAdminId(adminId);
+				bean.setUserName(rs.getString("userName"));
+				bean.setPassword(rs.getString("password"));
+				return bean;
+			} else {
+				return null;
+			}
+
+		} catch (SQLException e) {
+			System.err.println(e);
+			return null;
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+		}
+
+	}
+
+	public static boolean insert(Admin bean) throws Exception {
+
+		String sql = "INSERT into admin (userName, password) " +
+				"VALUES (?, ?)";
+		ResultSet keys = null;
+		try (
+				Connection conn = DBUtil.getConnection(DBType.MYSQL);
+				PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+				) {
+			
+			stmt.setString(1, bean.getUserName());
+			stmt.setString(2, bean.getPassword());
+			int affected = stmt.executeUpdate();
+			
+			if (affected == 1) {
+				keys = stmt.getGeneratedKeys();
+				keys.next();
+				int newKey = keys.getInt(1);
+				bean.setAdminId(newKey);
+			} else {
+				System.err.println("No rows affected");
+				return false;
+			}
+			
+		} catch (SQLException e) {
+			System.err.println(e);
+			return false;
+		} finally{
+			if (keys != null) keys.close();
+		}
+		return true;
+	}
+
 }
