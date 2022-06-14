10dfaef45931d690e230bf0eba344b2de67024fe
manojbagari <targetforias@gmail.com>
2022-06-14 10:46:29 +0530

    Use limit clause in sql instead of resultset.setMaxRows(..)

--- src/main/java/com/database/jdbc/Main.java
+++ src/main/java/com/database/jdbc/Main.java
@@ -5,38 +5,26 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import com.database.db.tables.States;
+import com.database.db.tables.Tours;
 
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 
-		try (Connection conn = DBUtil.getConnection(DBType.HSQLDB);
-				Statement stmt = conn.createStatement(
-						ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+//		ResultSet rs = null;
+		try (
+				Connection conn = DBUtil.getConnection(DBType.HSQLDB);
+				Statement stmt = conn.createStatement();
 				ResultSet rs = stmt.executeQuery(
-						"SELECT stateId, stateName FROM states");   	
+						"SELECT tourId, tourName, price FROM tours " +
+						"LIMIT 5, 5");
 				) {
-
-			States.displayData(rs);
-
-			rs.last();
-			System.out.println("Number of rows: " + rs.getRow());
-
-			rs.first();
-			System.out.println("The first state is " + rs.getString("stateName"));
-
-			rs.last();
-			System.out.println("The last state is " + rs.getString("stateName"));
-
-			rs.absolute(10);
-			System.out.println("The 10th state is " + rs.getString("stateName"));
-
-
+//			stmt.setMaxRows(5); // this is not ideal use limit clause in sql instead , as this fetches all rows and shows first five
+			Tours.displayData(rs);
 		} catch (SQLException e) {
 			System.err.println(e);
-		}
+		} 
 	}
-
 }
+
