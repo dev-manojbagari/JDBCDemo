8d3c71bd53459ad3e0b46a7b82631e219e95df27
manojbagari <targetforias@gmail.com>
2022-06-14 13:24:19 +0530

    Example of Fetching Table and Column Metadata

--- src/main/java/com/database/Main.java
+++ src/main/java/com/database/Main.java
@@ -3,6 +3,8 @@ package com.database;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+
 
 
 public class Main {
@@ -11,6 +13,8 @@ public class Main {
 
 		Connection conn = ConnectionManager.getInstance().getConnection();
 		ResultSet rsTables = null;
+		ResultSet rsColumns = null;
+		ArrayList<String> tables = new ArrayList<>();
 		
 		try  {
 			
@@ -18,14 +22,33 @@ public class Main {
 			String[] tableTypes = {"TABLE"};
 			rsTables = metadata.getTables(null, "%", "%", tableTypes);
 			while (rsTables.next()) {
-				System.out.println(rsTables.getString("TABLE_NAME"));
+				tables.add(rsTables.getString("TABLE_NAME"));
 			}
 
+			for (String tableName : tables) {
+				System.out.println("Table: " + tableName);
+				System.out.println("----------------");
+				
+				rsColumns = metadata.getColumns(null, "%", tableName, "%");
+				
+				while (rsColumns.next()) {
+					StringBuffer buffer = new StringBuffer();
+					buffer.append(rsColumns.getString("COLUMN_NAME"));
+					buffer.append(": ");
+					buffer.append(rsColumns.getString("TYPE_NAME"));
+					System.out.println(buffer.toString());
+				}
+				
+				System.out.println("");
+			}
+			
+			
 		} catch (Exception e) {
 			System.err.println(e);
 		}
 		finally {
 			rsTables.close();
+			rsColumns.close();
 		}
 		
 		ConnectionManager.getInstance().close();
