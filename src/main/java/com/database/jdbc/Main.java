3a0fc7dac195848a4426e543b5dea9fcd670b473
manojbagari <targetforias@gmail.com>
2022-06-14 06:08:49 +0530

    Replaced finally with try with resource(requires java 7 or above) for closing jdbc resource

--- src/main/java/com/database/jdbc/Main.java
+++ src/main/java/com/database/jdbc/Main.java
@@ -1,6 +1,5 @@
 package com.database.jdbc;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,39 +10,20 @@ public class Main {
 
 	private static final String USERNAME = "dbuser";
 	private static final String PASSWORD = "dbpassword";
-	private static final String CONN_STRING =
-			"jdbc:hsqldb:data/explorecalifornia";
-	
+	private static final String CONN_STRING = "jdbc:hsqldb:data/explorecalifornia";
+
 	public static void main(String[] args) throws SQLException {
-		
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		
-		try {
-			conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("SELECT coutn(*) FROM states");
-			
+
+		try (   Connection conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery("SELECT coutn(*) FROM states");) {
+
 			rs.next();
 			System.out.println("Number of rows: " + rs.getRow());
-			
+
 		} catch (SQLException e) {
 			System.err.println(e);
-		} finally {
-			if (rs != null) {
-				rs.close();
-			}
-			if (stmt != null) {
-				stmt.close();
-			}
-			if (conn != null) {
-				conn.close();
-			}
 		}
-		
+
 	}
-
 }
-
-
